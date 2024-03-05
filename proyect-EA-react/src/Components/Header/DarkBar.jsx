942e7e49bf9311debf2f3952cf9792086a86b042
Hadassa Fentanes <hadassafentanes@gmail.com>
2024-03-05 02:53:19 +0100

    Creación de la darkbar

--- proyect-EA-react/src/Components/Header/DarkBar.jsx
+++ proyect-EA-react/src/Components/Header/DarkBar.jsx
@@ -1,5 +1,129 @@
+import { useState } from "react";
 import "../../Styles/Header/darkbar.css";
 
+function DarkBarWrapperItem({ link, img, alt, text }) {
+  return (
+    <div className="darkbar">
+      <div className="item">
+        <a href={link}>
+          <img src={img} className="icon" alt={alt} />
+          <p>{text}</p>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+function DarkBarProfile() {
+  return (
+    <div className="section">
+      <div className="icons">
+        <div className="item-group">
+          <DarkBarWrapperItem
+            link="https://signin.ea.com/p/juno/login?execution=e206671001s1&initref=https%3A%2F%2Faccounts.ea.com%3A443%2Fconnect%2Fauth%3Finitref_replay%3Dfalse%26display%3DjunoWeb%2Flogin%26response_type%3Dcode%26redirect_uri%3Dhttps%3A%2F%2Fwww.ea.com%2Flogin_check%26locale%3Den_US%26client_id%3DEADOTCOM-WEB-SERVER"
+            img="./src/Media/header/svg/signin-icon.svg"
+            alt="A profile pic."
+            text="Sign In"
+          />
+          <DarkBarWrapperItem
+            link="https://signin.ea.com/p/juno/create?execution=e206671003s1&initref=https%3A%2F%2Faccounts.ea.com%3A443%2Fconnect%2Fauth%3Finitref_replay%3Dfalse%26display%3DjunoWeb%2Fcreate%26response_type%3Dcode%26redirect_uri%3Dhttps%3A%2F%2Fwww.ea.com%2Flogin_check%26locale%3Den_US%26client_id%3DEADOTCOM-WEB-SERVER"
+            img="./src/Media/header/svg/register-icon.svg"
+            alt="A profile pic with a plus sign."
+            text="Create Account"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function DarkBarHelp() {
+  return (
+    <div className="section  is-active">
+      <p className="bold">NEED HELP?</p>
+      <div className="icons">
+        <div className="item-group">
+          <DarkBarWrapperItem
+            link="https://help.ea.com/en/help/account/ea-login-verification-information/"
+            img="./src/Media/header/svg/medallion-icon.svg"
+            alt="A circle with an EA logo."
+            text="Verify my EA Login"
+          />
+          <DarkBarWrapperItem
+            link="https://help.ea.com/en/help/account/link-your-console-accounts/"
+            img="./src/Media/header/svg/link-icon.svg"
+            alt="A round square with chains."
+            text="Linking your platform accounts to your EA Account"
+          />
+        </div>
+        <div className="item-group">
+          <DarkBarWrapperItem
+            link="https://help.ea.com/en/help/faq/redeem-product-and-promotional-codes/"
+            img="./src/Media/header/svg/medallion-icon.svg"
+            alt="A circle with an EA logo."
+            text="How do I redeem a Code?"
+          />
+          <DarkBarWrapperItem
+            link="https://help.ea.com/en/help/faq/connection-troubleshooting-basic/"
+            img="./src/Media/header/svg/alert-icon.svg"
+            alt="A round square with a triangle with an exclamation mark inside."
+            text="I can't play online!"
+          />
+        </div>
+      </div>
+      <p>
+        Can't find what you're looking for? Go to{" "}
+        <a href="https://help.ea.com/en/help-home/">EA Help</a>,{" "}
+        <a href="https://answers.ea.com/t5/Answer-HQ-English/ct-p/AHQ-English">
+          Answers HQ
+        </a>{" "}
+        or <a href="https://www.ea.com/forums">EA Forums</a>
+      </p>
+    </div>
+  );
+}
+
+function DarkBarWrapper({ showContent }) {
+  return (
+    <div className="dark-bar-wrapper">
+      <span className="btn-close"></span>
+      {showContent === "profile" ? <DarkBarProfile /> : <DarkBarHelp />}
+    </div>
+  );
+}
+
 export default function DarkBar() {
-  return <div className="darkbar"></div>;
+  const [showContent, setShowContent] = useState("");
+
+  function handleContent(content) {
+    setShowContent(content);
+  }
+
+  return (
+    <div className="darkbar">
+      <img
+        onClick={() => {
+          handleContent("profile");
+        }}
+        className="btnDarkMenu profile-icon"
+        src="./src/Media/header/svg/profile-icon.svg"
+        alt="Profile"
+      />
+      <img
+        onClick={() => {
+          handleContent("help");
+        }}
+        className="btnDarkMenu help-icon"
+        src="./src/Media/header/svg/question-icon.svg"
+        alt="Question Mark"
+      />
+      <img
+        className="logo-icon"
+        src="./src/media/header/svg/logo-icon.svg"
+        alt="EA Logo"
+      />
+      <span className="overlay"></span>
+      <DarkBarWrapper showContent={showContent} />
+    </div>
+  );
 }
