d4a3986815dfaf9b7dffc1e08003601556a80346
Hadassa Fentanes <hadassafentanes@gmail.com>
2024-03-07 03:32:06 +0100

    feat(component): creation of the darkbar-item

--- src/components/Header/DarkBar.jsx
+++ src/components/Header/DarkBar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "../../styles/header/darkbar.css";
+import DarkBarWrapper from "./DarkBarItem";
 
 export default function DarkBar() {
   const [showContent, setShowContent] = useState("");
