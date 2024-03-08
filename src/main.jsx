b8b78ee0424217947bdde8e4ad70a3a2ea44d920
Hadassa Fentanes <hadassafentanes@gmail.com>
2024-03-08 13:17:48 +0100

    feat(component): darkbar-item updates, fonts added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header/Header";
 import "./styles/common/reset.css";
+import "./styles/common/fonts.css";
 import "./styles/common/root.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
