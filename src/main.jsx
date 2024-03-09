4eeb7847e24397138a4913c7c537e262f230862b
Hadassa Fentanes <hadassafentanes@gmail.com>
2024-03-09 20:20:56 +0100

    He arreglado el nombre de algunas carpetas

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./components/Header/Header";
+import HeaderSection from "./components/Header/HeaderSection";
 import "./styles/common/reset.css";
 import "./styles/common/fonts.css";
 import "./styles/common/root.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Header />
+    <HeaderSection />
   </React.StrictMode>
 );
