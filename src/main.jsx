32cd9b01cca76bd3d6653b6ae5955048467a61cc
Vincent <megazpot@gmail.com>
2024-03-18 14:08:32 +0100

    Created parent component Content1 for Banner1 and its under component. Added functional responsive background

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import HeaderSection from "./components/Header/HeaderSection";
-import HeroBanner1 from "./components/Banners/HeroBanner1";
+
 import CardHeader from "./components/Cards/CardHeader";
+import Content1 from "./components/Banners/Content1";
 
 import "./styles/common/reset.css";
 import "./styles/common/fonts.css";
@@ -11,7 +12,7 @@ import "./styles/common/root.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HeaderSection/>
-    <HeroBanner1 backgroundSrc="./src/media/banners/banner1/apex-1920w.jpg" logoSrc="./src/media/banners/banner1/apex-legends-breakout-logo.svg" />
+    <Content1/>
     <CardHeader/>
   </React.StrictMode>
 );
