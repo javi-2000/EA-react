f6990376f58dab860bf73d161418a95f11df2100
Vincent <megazpot@gmail.com>
2024-03-18 12:41:18 +0100

    Creation of branch and advancements on HeroBanner1

--- src/components/Banners/HeroBanner1.jsx
+++ src/components/Banners/HeroBanner1.jsx
@@ -1,11 +1,13 @@
+import Button from "../Utilities/button";
+
 export default function HeroBanner1 () {
 
     return(
         <div className="">
-            <img className="" src="" alt="" />
+            <img className="HeroBannerBackground1" src="" alt="" />
             <div>
                 <img src="" alt="" />
-                <button></button>
+                <Button className={'ButtonBanner1'} buttonText={'Play Now'}/>
             </div>
         </div>
     )
