6e3d785322a7f85f2474c1654e517208a81be097
Vincent <megazpot@gmail.com>
2024-03-18 12:34:51 +0100

    imageSrc finder added at services, progress  at HeroBanner1

---
+++ src/components/Banners/HeroBanner1.jsx
@@ -0,0 +1,12 @@
+export default function HeroBanner1 () {
+
+    return(
+        <div className="">
+            <img className="" src="" alt="" />
+            <div>
+                <img src="" alt="" />
+                <button></button>
+            </div>
+        </div>
+    )
+}
