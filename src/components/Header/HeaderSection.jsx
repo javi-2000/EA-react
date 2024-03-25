bd9c53e814fa41679eae0418e597a2e44d51644d
Hadassa Fentanes <hadassafentanes@gmail.com>
2024-03-25 23:32:40 +0100

    feat(component): creation of the lightbar

--- src/components/Header/HeaderSection.jsx
+++ src/components/Header/HeaderSection.jsx
@@ -1,10 +1,12 @@
 import HeaderDarkbar from "./HeaderDarkbar";
+import HeaderLightbar from "./HeaderLightbar";
 import "../../styles/header/header.css";
 
 export default function HeaderSection() {
   return (
     <header className="header">
       <HeaderDarkbar />
+      <HeaderLightbar />
     </header>
   );
 }
